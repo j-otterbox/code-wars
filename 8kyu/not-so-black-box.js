02921dc4d5780163da5a36f5ffa0d6ce7dd64615
Jason Otter <jasonotter17@gmail.com>
2022-05-18 15:20:14 -0700

    add new 8kyu solution

---
+++ 8kyu/not-so-black-box.js
@@ -0,0 +1,6 @@
+// The kata has been preloaded with a function called blackBox.
+// Figure out what the function does.
+// You may need to think laterally!
+
+// solution was to convert function code into string to see instructions
+JSopenSesame();
