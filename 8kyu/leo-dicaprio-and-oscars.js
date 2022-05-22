6d5a8c90a32834aa1703f3454d99b9e822de9dd5
Jason Otter <jasonotter17@gmail.com>
2022-05-21 22:14:07 -0700

    add new 8kyu solution

---
+++ 8kyu/leo-dicaprio-and-oscars.js
@@ -0,0 +1,8 @@
+function leo(oscar) {
+  if (oscar < 88)
+    return oscar === 86
+      ? "Not even for Wolf of wallstreet?!"
+      : "When will you give Leo an Oscar?";
+  else if (oscar === 88) return "Leo finally won the oscar! Leo is happy";
+  else return "Leo got one already!";
+}
