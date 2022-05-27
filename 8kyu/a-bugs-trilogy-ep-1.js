e99a56f904517384a25c4d4bf485a46439e7546d
Jason Otter <jasonotter17@gmail.com>
2022-05-26 18:14:39 -0700

    add new 8kyu solution

---
+++ 8kyu/a-bugs-trilogy-ep-1.js
@@ -0,0 +1,6 @@
+function yourFutureCareer() {
+  var rand = Math.random();
+  if (rand <= 0.32) return "FrontEnd Developer";
+  else if (rand <= 0.65) return "BackEnd Developer";
+  else return "Full-Stack Developer";
+}
